17c559b89a707b6b315d93bd7efbc8b6e13254af
OneTwo <gonchar.dev@yandex.ru>
2020-12-15 00:35:26 +0300

    add AddButtonList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import List from './components/List';
+import AddList from './components/AddList'
+
+import DB from "./assets/db.json";
 
 function App() {
   return (
@@ -38,20 +41,7 @@ function App() {
           ]}
             isRemovable
           />
-          <List items={[
-            {
-              className: "list__add-button",
-              icon: <svg width="12" height="12" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" >
-                <path d="M8 1V15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                <path d="M1 8H15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-              </svg>
-              ,
-
-              name: 'Добавить список',
-              // active: true,
-            }
-          ]}
-          />
+          <AddList colors={DB.colors} />
         </ul>
       </div>
       <div className="todo__tasks"></div>
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import classNames from 'classnames';
 
+import Badge from '../Badge'
+
 import './List.scss';
 
-function List({ items, isRemovable }) {
+function List({ items, isRemovable, onClick }) {
     return (
-        <ul className="list">
+        <ul onClick={onClick} className="list">
             {
                 items.map((item, index) => (
                     <li key={index} className={classNames(item.className, { 'active': item.active })}>
                         <i>
-                            {item.icon ? item.icon : <i className={`badge badge--${item.color}`}></i>}
+                            {item.icon ? item.icon : <Badge color={item.color} />}
                         </i>
                         <span>{item.name}</span>
                     </li>
